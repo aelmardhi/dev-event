b59a297e398be0fb4915556e729bd739a231c7f3
aelmardhi <aelmardhi@gmail.com>
2026-01-07 01:32:58 +0300

    feat: events route post add image to cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,5 +1,6 @@
 import { Event } from "@/database";
 import connectDB from "@/lib/mongodb";
+import { v2 as cloudinary } from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST( req: NextRequest){
@@ -14,6 +15,22 @@ export async function POST( req: NextRequest){
             return NextResponse.json({message: 'Invalid JSON data format'}, {status: 400})
         }
 
+        const file = formData.get('image') as File;
+        if(!file) return NextResponse.json({message: 'Image file is required'}, {status: 400});
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve,reject) =>{
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvents'}, (error, result)=>{
+                if(error)
+                    return reject(error);
+                resolve(result)
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as {secure_url: string} ).secure_url
+        
         const createdEvent = await Event.create(event);
         return NextResponse.json({message: 'Event created successfully', event: createdEvent}, {status: 201})
     }catch (e){
